2e5d5ffa3c45dea277b84a44954b3dead6557e74
Taufiqul Islam <tosunbd@gmail.com>
2021-03-23 17:48:10 +0600

    Third of the day

---
+++ wp-content/themes/aquila/assets/inc/classes/class-aquila-theme.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Bootstraps the Theme.
+ *
+ * @package Aquila
+ */
+?>
+
+
+<?php 
+
+namespace AQUILA_THEME\Inc;
+
+use AQUILA_THEME\Inc\Traits\Singleton;
+
+class AQUILA_THEME {
+	use Singleton;
+
+	protected function __construct() {
+
+		// load class.
+		$this->set_hooks();
+	}
+
+	protected function set_hooks() {
+		// actions and filters
+	}
+}
+
+?>
--- wp-content/themes/aquila/functions.php
+++ wp-content/themes/aquila/functions.php
@@ -9,6 +9,13 @@
 
 <?php 
 
+if (! defined( 'AQUILA_DIR_PATH' ))
+{
+    define( 'AQUILA_DIR_PATH', untrailingslashit( get_template_directory() ) );
+}
+
+require_once AQUILA_DIR_PATH . '/assets/inc/helpers/autoloader.php';
+
 // echo "<pre>";
 // //print_r( get_template_directory() . '/css/style.css' );
 // print_r(filemtime( get_template_directory() . '/style.css' ));
@@ -18,12 +25,12 @@
 function aquila_enqueue_scripts() 
 {
     // Register Styles
-    wp_register_style( 'style-css',  get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
-    wp_register_style( 'bootstrap-css',  get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [], false, 'all' );
+    wp_register_style( 'style-css',  get_stylesheet_uri(), [], filemtime( AQUILA_DIR_PATH . '/style.css' ), 'all' );
+    wp_register_style( 'bootstrap-css',  AQUILA_DIR_PATH . '/assets/src/library/css/bootstrap.min.css', [], false, 'all' );
 
     // Register Scripts
-    wp_register_style( 'bootstrap-css',  get_template_directory_uri() . '/assets/main.js', [], false, 'all' );    
-    wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true );
+    wp_register_style( 'bootstrap-css',  AQUILA_DIR_PATH . '/assets/main.js', [], false, 'all' );    
+    wp_register_script( 'bootstrap-js', AQUILA_DIR_PATH . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true );
     
     // Enqueue Styles
     wp_enqueue_style( 'style-css');
